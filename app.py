4940f3033b5e674b6ce7cf456563fbc41e7a487c
Krishna-baniya <baniyakrishna96@gmail.com>
2024-12-19 05:23:50 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+  File "main.py", line 1
+    from flask import Flask, request, jsonify, render_template
+    ^
+IndentationError: unexpected indent
+
+
+** Process exited - Return Code: 1 **
+Press Enter to exit terminal
